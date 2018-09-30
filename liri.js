9477a6d26b8c9188856ee6fa9cf49aedf03e4212
Castracani <willmyers993@gmail.com>
2018-09-29 20:37:45 -0400

    installing packages and requiring them in liri.js

--- liri.js
+++ liri.js
@@ -3,3 +3,9 @@ require("dotenv").config();
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+var request=require("request");
+var fs = require("fs");
+
+var keys = require("./keys");
+
+var spot
